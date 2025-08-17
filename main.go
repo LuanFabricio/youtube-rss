1bd030a5340477be273e0668af1f6f471652e480
Luan Fabrício <luanlimaleite@hotmail.com>
2025-08-17 13:07:52 -0300

    refactor: moves API integration from `main.go` to `youtube.go`

--- main.go
+++ main.go
@@ -1,59 +1,20 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"time"
 )
 
-type ThumbnailInfo struct {
-	URL string `json:"url"`
-	Width int `json:"width"`
-	Height int `json:"height"`
-}
-
-type Thumbnail struct {
-	Default *ThumbnailInfo `json:"default"`
-	Medium *ThumbnailInfo `json:"medium"`
-	High *ThumbnailInfo `json:"high"`
-	Standart *ThumbnailInfo `json:"standart"`
-	MaxRes *ThumbnailInfo `json:"maxres"`
-}
-
-type ItemInfo struct {
-	PublishedAt time.Time `json:"publishedAt"`
-	ChannelId string `json:"channelId"`
-	Title string `json:"title"`
-	Description string `json:"description"`
-}
-
-type Item struct {
-	Kind string `json:"kind"`
-	Etag string `json:"etag"`
-	Id string `json:"id"`
-	Snippet ItemInfo `json:"snippet"`
-}
-
-type Body struct {
-	Kind string `json:"kind"`
-	Etag string `json:"etag"`
-	Id string `json:"id"`
-	Items []Item `json:"items"`
-}
-
 func main() {
-	run([]string{"add", "the office"})
-	run([]string{"add", "the office", "idk2"})
-	run([]string{"list-playlists"})
-	run([]string{""})
-	run([]string{"rm", "idk2"})
-	run([]string{"rm", "1"})
-	run([]string{"rm"})
 
-	os.Exit(1)
+	// run([]string{"add", "the office"})
+	// run([]string{"add", "the office", "idk2"})
+	// run([]string{"list-playlists"})
+	// run([]string{""})
+	// run([]string{"rm", "idk2"})
+	// run([]string{"rm", "1"})
+	// run([]string{"rm"})
+
 	LoadEnv()
 
 	key := os.Getenv("KEY")
@@ -61,28 +22,5 @@ func main() {
 		log.Panicln("Invalid KEY")
 	}
 
-	const BASE_URL string = "https://www.googleapis.com" +
-		"/youtube/v3/playlistItems" +
-		"?part=snippet&maxResults=50&playlistId=PL2Fq-K0QdOQiJpufsnhEd1z3xOv2JMHuk"
-
-	url := fmt.Sprintf("%v&key=%v", BASE_URL, key)
-	fmt.Println(url)
-
-	res, err := http.Get(url)
-	LogError(err)
-
-	var resBody Body
-	err = json.NewDecoder(res.Body).Decode(&resBody)
-
-	LogError(err)
-
-	for i, item := range resBody.Items {
-		fmt.Printf(
-			"[%v] Episode %v: %v (%v)\n",
-			item.Id,
-			len(resBody.Items) - i,
-			item.Snippet.Title,
-			item.Snippet.PublishedAt,
-		)
-	}
+	run(os.Args)
 }
---
+++ youtube.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type ThumbnailInfo struct {
+	URL string `json:"url"`
+	Width int `json:"width"`
+	Height int `json:"height"`
+}
+
+type Thumbnail struct {
+	Default *ThumbnailInfo `json:"default"`
+	Medium *ThumbnailInfo `json:"medium"`
+	High *ThumbnailInfo `json:"high"`
+	Standart *ThumbnailInfo `json:"standart"`
+	MaxRes *ThumbnailInfo `json:"maxres"`
+}
+
+type ItemInfo struct {
+	PublishedAt time.Time `json:"publishedAt"`
+	ChannelId string `json:"channelId"`
+	Title string `json:"title"`
+	Description string `json:"description"`
+}
+
+type Item struct {
+	Kind string `json:"kind"`
+	Etag string `json:"etag"`
+	Id string `json:"id"`
+	Snippet ItemInfo `json:"snippet"`
+}
+
+type Body struct {
+	Kind string `json:"kind"`
+	Etag string `json:"etag"`
+	Id string `json:"id"`
+	Items []Item `json:"items"`
+}
+
+const BASE_URL string = "https://www.googleapis.com" +
+	"/youtube/v3/playlistItems" +
+	"?part=snippet&maxResults=50"
+
+func GetPlaylist(apiKey string, playlistId string) (Body, error) {
+	url := BASE_URL + fmt.Sprintf("&playlistId=%v", playlistId) +
+		fmt.Sprintf("&key=%v", apiKey)
+
+	res, err := http.Get(url)
+	LogError(err)
+
+	var resBody Body
+	err = json.NewDecoder(res.Body).Decode(&resBody)
+
+	return resBody, err
+}
