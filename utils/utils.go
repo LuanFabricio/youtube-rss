d54e7eaf191e15d7d24c8cb7ba3673f2fa719a28
Luan Fabrício <luanlimaleite@hotmail.com>
2025-08-24 20:23:48 -0300

    feat: creates GetBaseFolder and CreateBaseFolderIfNotExists

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,28 @@
 package utils
 
-import "log"
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
 
 func LogError(err error) {
 	if err != nil {
 		log.Panicf("Error: %v\n", err)
+	}
+}
+
+func GetBaseFolder() string {
+	user_folder, err := os.UserHomeDir()
+	LogError(err)
+
+	return filepath.Join(user_folder, ".youtube-rss")
+}
+
+func CreateBaseFolderIfNotExists() {
+	modes := os.ModeDir | os.ModePerm
+	err := os.Mkdir(GetBaseFolder(), modes)
+	if !os.IsExist(err) {
+		LogError(err)
 	}
 }
