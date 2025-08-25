5c562efc12c4f79ab447e9c7564ad3652e0bba9f
Luan Fabrício <luanlimaleite@hotmail.com>
2025-08-25 20:47:30 -0300

    feat: implements flags module
    Creates just-update and show-all-unwatched flags and implements registerFlags and applyFlags functions

---
+++ cli/flags.go
@@ -0,0 +1,64 @@
+package cli
+
+import (
+	"os"
+	db "youtube-rss/database"
+)
+
+type FlagCallback func(args []string) []string
+
+type Flag struct {
+	name string
+	shortname string
+	callback FlagCallback
+}
+
+var flags []Flag = []Flag{
+	{
+		name: "just-update",
+		shortname: "ju",
+		callback: func(args []string) []string {
+			os.Exit(0)
+			return []string{}
+		},
+	},
+	{
+		name: "show-all-unwatched",
+		shortname: "sau",
+		callback: func(args []string) []string {
+			queries := []string{}
+
+			playlists := db.GetPlaylists()
+
+			for _, playlist := range playlists {
+				queries = append(queries, "show-unwatched", playlist.Name)
+			}
+
+			return queries
+		},
+	},
+}
+
+func registerFlags() map[string]Flag {
+	flagsMap := make(map[string]Flag)
+
+	for _, flag := range flags {
+		flagsMap["--"+flag.name] = flag
+		flagsMap["-"+flag.shortname] = flag
+	}
+
+	return flagsMap
+}
+
+func applyFlags(args []string) []string {
+	flagsMap := registerFlags()
+
+	for i := range args {
+		flag, found := flagsMap[args[i]]
+		if found {
+			args = flag.callback(args)
+		}
+	}
+
+	return args
+}
